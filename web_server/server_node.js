f4072b8a22a4130f2125dfdcc09a343777223aaf
pranmandale <pranavmandale2025@gmail.com>
2024-12-17 11:20:52 +0530

    first node server created

---
+++ web_server/server_node.js
@@ -0,0 +1,26 @@
+const http = require('http');
+
+const hostname = '127.0.0.1'
+const port = 3000
+
+const server = http.createServer((req,res) => {
+   if (req.url === '/') {
+     res.statusCode = 200
+     res.setHeader('Content-Type','text/plain')
+     res.end("Hello This is Pranav Mandale")
+   } 
+   else if (req.url === '/pranav-developer') {
+    res.statusCode = 200
+    res.setHeader('Content-Type','text/plain')
+    res.end("This is Pranav Mandale backend developer")
+  }
+  else {
+    res.statusCode = 404
+     res.setHeader('Content-Type','text/plain')
+     res.end("404 not found")
+  }
+})
+
+server.listen(port, hostname,() => {
+    console.log(`server is listening on port http://${hostname}:${port}`)
+})
